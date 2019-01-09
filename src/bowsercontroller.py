c1051964a81584dbdc63673550d5ceb8ffb9d198
Adriano Gil <adrianogil.san@gmail.com>
2019-01-09 09:53:14 -0400

    Save search engine

--- src/bowsercontroller.py
+++ src/bowsercontroller.py
@@ -1,11 +1,15 @@
 import webbrowser
 import utils
 
+import json
+import os
+
+
 class BowserController():
     """BowserController is responsible for managing commands"""
     def __init__(self):
-        pass
-        
+        self.search_engine_data_path = os.environ["BOWSER_DIRS"] + "/data/search_engine.bowser"
+
     def open(self, args, extra_args):
         url = args[0]
 
@@ -16,10 +20,48 @@ class BowserController():
         webbrowser.open(url, autoraise=True)
         # webbrowser.open_new_tab(url)
 
+    def search_engine(self, args, extra_args):
+        if not os.path.exists(self.search_engine_data_path):
+            return
+
+        keyword = args[0]
+
+        with open(self.search_engine_data_path, 'r') as f:
+            search_engine_data = json.load(f)
+
+        if keyword not in search_engine_data:
+            return
+
+        search_engine_str = search_engine_data[keyword]
+
+        for i in range(1, len(args)):
+            search_engine_str = search_engine_str.replace("%s" + str(i - 1), args[i])
+
+        self.open([search_engine_str], [])
+
+    def save_search_engine(self, args, extra_args):
+        search_engine_keyword = args[0]
+        search_engine_str = args[1]
+
+        search_engine_data = {}
+
+        if os.path.exists(self.search_engine_data_path):
+            with open(self.search_engine_data_path, 'r') as f:
+                search_engine_data = json.load(f)
+
+        search_engine_data[search_engine_keyword] = search_engine_str
+
+        with open(self.search_engine_data_path, 'w') as f:
+            json.dump(search_engine_data, f)
+
     def get_commands(self):
         commands_parse = {
-            '-o'           : self.open,
-            '--open'       : self.open,
+            '-o'              : self.open,
+            '-e'              : self.search_engine,
+            '-s'              : self.save_search_engine,
+            '--open'          : self.open,
+            '--engine'        : self.search_engine,
+            '--save'          : self.save_search_engine,
             # '-s'           : self.save,
             # '-r'           : self.remove,
             # '-u'           : self.update,
